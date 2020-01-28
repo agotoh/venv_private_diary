64d7f7346329e40addd340a5d76d9ec315475709
agotoh <agotoh@efficient.co.jp>
2020-01-28 14:58:36 +0900

    modify index

--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-# from django.views import generic
+from django.views import generic
 from django.template import loader
 from django.http import HttpResponse
 
 
-def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    template = loader.get_template('index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+# def index(request):
+#     # return HttpResponse("Hello, world. You're at the polls index.")
+#     template = loader.get_template('index.html')
+#     context = {}
+#     return HttpResponse(template.render(context, request))
+
+class IndexView(generic.TemplateView):
+    template_name = "index.html"
+
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+#     form_class = InquiryForm
