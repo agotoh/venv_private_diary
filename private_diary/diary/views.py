241849baa10a78f20ad9519d9d9679530cf499be
agotoh <agotoh@efficient.co.jp>
2020-02-04 11:02:39 +0900

    diary_detail 実装

--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -37,3 +37,8 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
         diaries = Diary.objects.filter(
             user=self.request.user).order_by('-created_at')
         return diaries
+
+
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Diary
+    template_name = 'diary_detail.html'
